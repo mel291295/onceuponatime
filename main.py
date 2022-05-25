fe28f3e7596f6c667bbed9bafc564f34bf55dddf
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-25 13:56:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from streamlit import st
+import pandas as pd
+import streamlit as st
+
+
+st.title("Once upon a time")
 
 uploaded_file = st.file_uploader("/content/grimms_fairytales.csv")
 if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
-
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-     st.write(stringio)
-
-     # To read file as string:
-     string_data = stringio.read()
-     st.write(string_data)
-
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
+  df = pd.read_csv(uploaded_file)
+  st.write(dataframe)
