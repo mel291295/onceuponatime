03a86a9313675576cbc8db49eb77aa35bea029c5
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-25 14:53:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,29 @@ import streamlit as st
 
 st.title("Once upon a time")
 
+import os
+from gtts import gTTS
+from googletrans import Translator
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
+from  PIL import Image
+import jieba
+from xpinyin import Pinyin
 
-uploaded_file = st.file_uploader(“"C:\Users\ASUS\Documents\fairytales.xlsx"”)
-if uploaded_file is not None:
-#read csv
-df1=pd.read_csv(uploaded_file)
+try:
+    os.mkdir("temp_folder")
+except:
+    pass
+
+#Add a logo (optional) in the sidebar
+logo = Image.open(r'"C:\Users\ASUS\Desktop\Project LAB\Multilingual2x.webp"')
+st.sidebar.image(logo,  width=120)
+
+#Add the expander to provide some basic information about the app
+st.sidebar.markdown("### My Little Language learning App")
+with st.sidebar.expander("About the App"):
+     st.write("""
+        This language learning App is built by Sharone Li using Streamlit. Check out her medium article for more details about the app and source code.
+     """)
+st.sidebar.markdown("You can build your own language learning app using streamlit and Google Translate API. \n\n I only showed a few languages in this app, but you can certainly expand to other languages as well. Have fun!")
+
