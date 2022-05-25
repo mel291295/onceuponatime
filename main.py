c0ea63f37ce02c843ad33575305a820d6bf7b1ce
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-25 14:44:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.title("Once upon a time")
 
-uploaded_file = st.file_uploader("/content/grimms_fairytales.csv")
+uploaded_file = st.file_uploader("https://github.com/mel291295/onceuponatime/blob/main/fairytales.xlsx")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(dataframe)
